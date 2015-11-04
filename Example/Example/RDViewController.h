a239fe9ad46d7476e1b9f0905781821ee1946665
Vitali Kurlovich <vitalikurlovich@gmail.com>
2015-11-04 13:48:14 +0300

    update example

---
+++ Example/Example/RDViewController.h
@@ -0,0 +1,13 @@
+//
+//  RDViewController.h
+//  RDActionKit
+//
+//  Created by Vitali Kurlovich on 11/02/2015.
+//  Copyright (c) 2015 Vitali Kurlovich. All rights reserved.
+//
+
+@import UIKit;
+
+@interface RDViewController : UIViewController
+
+@end
---
+++ Example/Example/RDViewController.m
@@ -0,0 +1,73 @@
+//
+//  RDViewController.m
+//  RDActionKit
+//
+//  Created by Vitali Kurlovich on 11/02/2015.
+//  Copyright (c) 2015 Vitali Kurlovich. All rights reserved.
+//
+
+#import "RDViewController.h"
+
+#import <RDActionKit/RDActionKit.h>
+
+
+@interface RDViewController ()
+@property (nonatomic) IBOutlet UIButton* button;
+@property (nonatomic) IBOutlet UISegmentedControl* segmentedControl;
+@end
+
+@implementation RDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    RDActionItem* actionItem = [[RDActionItem alloc] initWithAction:[RDAction actionWithBlock:^{
+        NSLog(@"OnAction");
+    }] text:@"Action 1" image:[UIImage imageNamed:@"star"]];
+    
+    RDActionItem* actionItem2 = [[RDActionItem alloc] initWithAction:[RDAction actionWithBlock:^{
+        NSLog(@"OnAction 2");
+    }] text:@"Action 2"];
+    
+    [self.button setRDActionItem:actionItem];
+    
+    [self.segmentedControl setRDActionItem:actionItem forSegmentAtIndex:0];
+    [self.segmentedControl setRDActionItem:actionItem2 forSegmentAtIndex:1];
+    
+    
+    self.toolbarItems = @[ [[UIBarButtonItem alloc] initWithActionItem:actionItem style:UIBarButtonItemStyleDone],
+                           [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc] initWithActionItem:actionItem2 style:UIBarButtonItemStylePlain],
+                           ];
+    
+    __weak typeof(self) weakSelf = self;
+    
+    
+    
+    UIBarButtonItem* barItems = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:nil action:nil];
+    
+    self.navigationItem.rightBarButtonItem =  barItems;
+    
+    
+    [barItems setOnActionBlock:^(UIBarButtonItem *barButtonItem) {
+        
+        UIAlertController* controller = [UIAlertController alertControllerWithTitle:@"Alert" message:@"Message" preferredStyle:UIAlertControllerStyleActionSheet];
+        
+        [controller addAction:[UIAlertAction actionWithRDActionItem:actionItem style:UIAlertActionStyleDefault]];
+        
+        [controller addAction:[UIAlertAction actionWithRDActionItem:actionItem2 style:UIAlertActionStyleDefault]];
+        
+        [weakSelf presentViewController:controller animated:YES completion:nil];
+        
+    }];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self.navigationController setToolbarHidden:NO];
+}
+
+@end
